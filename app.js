4614c406f01ab626a8826e6d0ab7a74484ab4f70
Luana DNS <ludiasnsilva@gmail.com>
2023-01-30 20:08:29 +0000

    getting error

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var listsRouter = require("./routes/lists");
 
 var app = express();
 
@@ -15,8 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/lists", listsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import { text } from "express";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+	<h1>OI EU FUNCIONO</h1>;
+}
+
+export default App;
