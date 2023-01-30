c58424286fa83410145f5762c98b52c7306e2fd2
Luana DNS <ludiasnsilva@gmail.com>
2023-01-30 20:50:31 +0000

    fixed error

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
-import { text } from "express";
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-	<h1>OI EU FUNCIONO</h1>;
+	return <h1>OI EU FUNCIONO</h1>;
 }
 
 export default App;
